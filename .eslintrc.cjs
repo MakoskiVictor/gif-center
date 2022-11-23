d80092fb87e2d7a59b4f1ac27bacb7d37c17e7c2
Makoski Víctor <makoski.ed@gmail.com>
2022-11-23 16:15:46 -0300

    typescript and eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  /* extends: ['plugin:react/recommended', 'standard-with-typescript'], */
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  /* plugins: ['react'], */
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/ban-types': 'error',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/consistent-type-definitions': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+   /*  noUnusedLocals: true,
+    noUnusedParameters: true */
+
+  }
+}
