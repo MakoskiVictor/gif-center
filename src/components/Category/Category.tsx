111cef7baf564a07de2bc7f59681135b32cec3cc
MakoskiVictor <makoski.ed@gmail.com>
2023-01-05 19:43:38 -0300

    trendings and categories

---
+++ src/components/Category/Category.tsx
@@ -0,0 +1,10 @@
+export default function Category ({name, options = []}) {
+    return(
+        <div>
+            <div className="title"> {name} </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,1 +1,3 @@
 export const API_KEY = 'kI2OwJlo3bGk1RqEKyGgVY5wMe5X7KDn'
+
+export const GIPHY_BASE_URL = 'https://api.giphy.com/v1/gifs/'
